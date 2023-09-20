def6f4444b36a30e735bed8a4e570823426d98cf
DarrenPaulWright <darren@darrenpaulwright.com>
2023-09-20 13:28:00 -0700

    Fixing hanging script in LogScale

--- src/axis/LogScale.ts
+++ src/axis/LogScale.ts
@@ -4,25 +4,27 @@ import { integerDigits, round } from 'type-enforcer-math';
 import type { INumericDomain } from '../types';
 import Scale from './Scale.js';
 
-const log = (value: number): number => value === 0 ? 0 : Math.log(value);
-
-const mean = (a: number, b: number): number => {
-	return Math.exp((log(a) + log(b)) / 2);
-};
-
-const snap = (value: number, offset = 0): number => {
-	return Math.pow(10, integerDigits(value) + offset);
-};
-
 export default class LogScale extends Scale {
 	private _scaleValue = 0;
+
+	private log(value: number): number {
+		return value <= 0 ? 0 : Math.log(value);
+	}
+
+	private mean(a: number, b: number): number {
+		return Math.exp((this.log(a) + this.log(b)) / 2);
+	}
+
+	private snap(value: number, offset = 0): number {
+		return Math.pow(10, integerDigits(value) + offset);
+	}
 
 	private isBigEnough(a: number, b: number): boolean {
 		return this.getCharOffset(b) - this.getCharOffset(a) > this.minTickOffset;
 	}
 
 	private divideRange(rangeStart: number, rangeEnd: number, ticks: Array<number>): void {
-		const mid = round(mean(rangeStart, rangeEnd), 2, 2);
+		const mid = round(this.mean(rangeStart, rangeEnd), 2, 2);
 
 		if (this.isBigEnough(rangeStart, mid)) {
 			ticks.push(mid);
@@ -37,18 +39,24 @@ export default class LogScale extends Scale {
 		const domain = this.domain as INumericDomain;
 
 		if (this.shouldGetStart) {
-			snapped = snap(domain[0], -2);
+			snapped = this.snap(domain[0], -2);
 
 			this.start = Math.floor(domain[0] / snapped) * snapped;
 		}
 
 		if (this.shouldGetEnd) {
-			snapped = snap(domain[1], -2);
+			snapped = this.snap(domain[1], -2);
 
 			this.end = Math.ceil(domain[1] / snapped) * snapped;
+
+			if (this.end < this.start) {
+				const end = this.end;
+				this.end = this.start;
+				this.start = end;
+			}
 		}
 
-		this._scaleValue = ((this.size - 1) / (log(this.end) - log(this.start))) || 1;
+		this._scaleValue = ((this.size - 1) / (this.log(this.end) - this.log(this.start))) || 1;
 	}
 
 	ticks(): Array<number> {
@@ -56,10 +64,10 @@ export default class LogScale extends Scale {
 		const end = this.end;
 		const ticks = [];
 		let pos = start;
-		let tick = snap(start);
+		let tick = this.snap(start);
 
 		while (!this.isBigEnough(start, tick)) {
-			tick = snap(tick);
+			tick = this.snap(tick);
 		}
 
 		if (tick > end) {
@@ -76,7 +84,7 @@ export default class LogScale extends Scale {
 				pos = tick;
 			}
 
-			tick = snap(tick);
+			tick = this.snap(tick);
 		}
 
 		ticks.push(end);
@@ -92,11 +100,11 @@ export default class LogScale extends Scale {
 			value === this.end;
 	}
 
-	getCharOffset(value: number, fractionDigits = 0): number {
+	getCharOffset(value: number, desiredFractionDigits = 0): number {
 		return round(
-			((log(value) - log(this.start)) * this._scaleValue) + 0.5,
-			fractionDigits
+			((this.log(value) - this.log(this.start)) * this._scaleValue) + 0.5,
+			desiredFractionDigits
 		) ||
-			round(0.5, fractionDigits);
+			round(0.5, desiredFractionDigits);
 	}
 }
